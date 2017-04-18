8516adf2033ebf999977b09d6fabbbfee90f87bd
Jiheng Gao <love0.0chen@gmail.com>
2017-04-18 10:45:45 +0800

    重构

--- js-tree-builder.js
+++ js-tree-builder.js
@@ -1,71 +1,76 @@
-		function TreeBuild(list, options){
-			this.origin_list = list;
-			this.options = options;
-			this.options.primary_key = this.options.primary_key || 'id';
-			this.options.parent_key = this.options.parent_key || 'parent_id';
-		}
+function TreeBuild(list, options){
+	this.origin_list = JSON.parse(JSON.stringify(list));
+	this.options = options || {};
+	this.options.primary_key = (this.options && this.options.primary_key) || 'id';
+	this.options.parent_key = (this.options && this.options.parent_key) || 'parent_id';
+	this.processed_count = 0;
+	this.node_count = list.length;
+	this.compute();
+}
 
-		TreeBuild.prototype.getAllNodes = function(){
-			this.nodes = [].concat(this.getRootNodes());
-			this.appendChildrenForRootNodes();
-			return this.nodes;
-		};
+TreeBuild.prototype.getAllNodes = function(){
+	return this.nodes ? this.nodes : this.compute();
+};
 
-		TreeBuild.prototype.getRootNodes = function(){
-			var rootNodes = [];
-			for( var i = 0, m=this.origin_list.length; i < m; i++ ){
-				if( this.isRoot(this.origin_list[i][this.options.parent_key]) ){
-					rootNodes = rootNodes.concat(this.origin_list.splice(i, 1));
-					m=this.origin_list.length;
-				}
-			}
-			return rootNodes;
-		};
+TreeBuild.prototype.compute = function(){
+	this.nodes = this.getRootNodes();
+	this.decorateNodeItemList(this.nodes);
+	this.nodes.forEach(function(rootNode){
+		this.appendChildren(rootNode, this.getChildren(rootNode));
+	}.bind(this));
 
-		TreeBuild.prototype.isRoot = function(parent_id){
-			if( !parent_id ) return true;
-			for( var i = 0, n = this.origin_list.length; i < n; i++ ){
-				if( this.origin_list[i][this.options.primary_key] === parent_id ){
-					return false;
-				}
-			}
+	return this.nodes;
+};
+
+TreeBuild.prototype.getRootNodes = function(){
+	return this.origin_list.filter(function(nodeItem){
+		if ( !nodeItem[this.options.parent_key] || nodeItem[this.options.parent_key] === -1 ){
+			this.processed_count++;
 			return true;
-		};
-		TreeBuild.prototype.appendChildrenForRootNodes = function(){
-			for( var j = 0, n = this.nodes.length; j < n; j++ ){
-				this.appendChildrenForNodes(this.origin_list, this.nodes[j]);
-			}
-		};
-		TreeBuild.prototype.appendChildrenForNodes = function(node_list, current_node){
-			var children = node_list.filter(function(node_item){
-				return node_item[this.options.parent_key] === current_node[this.options.primary_key];
-			}.bind(this));
-			var siblings = node_list.filter(function(node_item){
-				return node_item[this.options.parent_key] !== current_node[this.options.primary_key];
-			}.bind(this));
+		}else{
+			return false;
+		}
+	}.bind(this));
+};
 
-			if(children.length){
-				current_node.children = current_node.children || [];
-				current_node.children = current_node.children.concat(children);
-				for( var i = 0, n = children.length; i<n; i++ ){
-					this.appendChildrenForNodes(siblings, children[i]);
-				}
-			}
-		};
+TreeBuild.prototype.getChildren = function(parentNode){
+	if( this.processed_count === this.node_count ){
+		return false;
+	}
+	return this.origin_list.filter(function(nodeItem){
+		if( nodeItem[this.options.parent_key] === parentNode[this.options.primary_key] ){
+			this.processed_count++;
+			nodeItem.parentNode = parentNode;
+			this.appendChildren(nodeItem, this.getChildren(nodeItem));
+			return true;
+		}else{
+			return false;
+		}
+	}.bind(this));
+};
 
-		TreeBuild.prototype.getParent = function(list, parent_id){
-			 var result;
-			 for(var i = list.length - 1;i>-1;i--){
-			 	if( list[i][this.options.primary_key] === parent_id){
-			 		return list[i];
-			 	}else {
-			 		if(Array.isArray(list[i].children) && list[i].children.length ){
-			 			result = hasParent(list[i].children, parent_id);
-			 			if( result ){
-			 				return result;
-			 			}
-			 		}
-			 	}
-			 }
-			 return false;
-		};
+TreeBuild.prototype.appendChildren = function(nodeItem, foundChildren){
+	if( !Array.isArray(foundChildren) || !foundChildren.length ){
+		return false;
+	}
+
+	if( Array.isArray(nodeItem.children) ){
+		nodeItem.children = nodeItem.children.concat(foundChildren);
+	}else{
+		nodeItem.children = foundChildren;
+	}
+	this.decorateNodeItemList(nodeItem.children);
+	return nodeItem.children;
+};
+
+TreeBuild.prototype.decorateNodeItemList = function(nodeItemList){
+	nodeItemList.forEach(function(nodeItem){
+		nodeItem.is_first = false;
+		nodeItem.is_last = false;
+	});
+	nodeItemList[0].is_first = true;
+	nodeItemList[nodeItemList.length - 1].is_last = true;
+}
+TreeBuild.prototype.isRoot = function(nodeItem){
+	return !nodeItem[this.options.parent_key] || nodeItem[this.options.parent_key] === -1
+};
